ac95069250301f97befa4b8f21d085fcd7f01a7d
xoo <bchevriaut@gmail.com>
2014-05-14 13:02:02 +0200

    Installation de Bower, Mise en place basique de Flask

--- Web/home.py
+++ Web/home.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<label>Entrer  le nom du fichier</label><input type="text" style="width:150px"><input type="submit" value="Envoi">'
+    return '<label>Entrer  le nom d u fichier</label><input type="text" style="width:150px"><input type="submit" value="Envoi">'
 
 if __name__ == '__main__':
     app.run()
