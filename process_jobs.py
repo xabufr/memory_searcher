99e758a3e044eb82f5192ad354164365633cced4
zequiel <aramel@epsi.fr>
2014-05-14 11:51:52 +0200

    Ajout des metadata dans elasticsearch

--- process_jobs.py
+++ process_jobs.py
@@ -6,4 +6,5 @@ jobs = Jobs()
 text_extractor = Tika()
 es = Elasticsearch()
 for job in jobs.iterate():
-    es.index(index="memory", doc_type="pdf", body={"content": text_extractor.get_file_content(job)})
+    es.index(index="memory", doc_type="pdf",
+             body={"content": text_extractor.get_file_content(job), "metadata": text_extractor.get_file_metadata(job)})
