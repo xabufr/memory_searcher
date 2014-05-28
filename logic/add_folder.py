3adab9da39903b6a523f54a9d9e3ba5575907636
Thomas Loubiou <xabufr@gmail.com>
2014-05-28 11:03:48 +0200

    Indexation des pdf seulement

--- logic/add_folder.py
+++ logic/add_folder.py
@@ -9,6 +9,6 @@ jobs = Jobs()
 
 for folder in folders:
     files_names = os.listdir(folder)
-    files = [os.path.join(folder, file_name) for file_name in files_names if os.path.splitext(file_name)[1] in (".doc", ".docx", ".pdf")]
+    files = [os.path.join(folder, file_name) for file_name in files_names if os.path.splitext(file_name)[1] == ".pdf"]
     for file_path in files:
         jobs.add_job(file_path)
